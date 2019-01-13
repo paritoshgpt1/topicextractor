b028dec759b5f1ba4d4cf061c0ef7f1ccc348027
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2019-01-13 16:02:44 -0500

    updated message

--- Extractor.py
+++ Extractor.py
@@ -4,7 +4,7 @@ from WebScraper import WebScraper
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Invalid Number of Arguments. Please enter a URL")
+        print("Invalid Number of Arguments. Please refer to the README for usage instructions.")
     else:
         url = sys.argv[1]
         scraper = WebScraper(url)
